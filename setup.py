48f62547af9e9617c57622adc741282648541dd3
timcera <timcera>
2007-03-04 02:54:54 +0000

    Stupid ez_setup.

--- setup.py
+++ setup.py
@@ -29,12 +29,13 @@ EXAMPLES:
     python setup.py sdist
 """
 
-# I REALLY don't like ez_setup - but when in Rome...
-import ez_setup
-ez_setup.use_setuptools()
-
 #===imports=============
-import sys,string
+import os,sys,re,string,getopt,shutil,commands
+# Let's see if ez_setup is installed
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 #===globals======
 modname='setup'
@@ -99,3 +100,27 @@ def main():
            packages = packages,
 	   package_data = {'tappy_lib' : ['astrolabe/data/vsop87d.txt']},
            )
+#==============================
+if __name__ == '__main__':
+    opts,pargs=getopt.getopt(sys.argv[1:],'hv',
+                 ['help','version','exec-prefix'])
+    for opt in opts:
+        if opt[0]=='-h' or opt[0]=='--help':
+            usage()
+            sys.exit(0)
+        elif opt[0]=='-v' or opt[0]=='--version':
+            print modname+": version="+version_text
+        elif opt[0]=='--exec-prefix':
+            exec_prefix=opt[1]
+
+    for arg in pargs:
+        if arg=='test':
+            do_test()
+            sys.exit(0)
+        elif arg=='doc':
+            do_doc()
+            sys.exit(0)
+        else:
+            pass
+
+    main()
