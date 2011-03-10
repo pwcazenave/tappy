5a0508f14ed54c802cff00db4a3ee77387405406
timcera <timcera>
2011-03-10 20:19:46 +0000

    * For the time being can only use long options - until/if I add to baker.

--- example/example.sh
+++ example/example.sh
@@ -4,13 +4,13 @@
 ../tappy.py --filter usgs mayport_florida_8720220_data.txt mayport_florida_8720220_data_def.txt
 
 # Read from COOPS web site data for 872-0218 Mayport Bar Pilots Dock
-../tappy.py --no-inferred 'http://tidesandcurrents.noaa.gov/data_menu.shtml?bdate=20020101&edate=20020401&wl_sensor_hist=W2&relative=&datum=6&unit=0&shift=g&stn=8720218&type=Historic+Tide+Data&format=View+Data' tidesandcurrents.def
+../tappy.py 'http://tidesandcurrents.noaa.gov/data_menu.shtml?bdate=20020101&edate=20020401&wl_sensor_hist=W2&relative=&datum=6&unit=0&shift=g&stn=8720218&type=Historic+Tide+Data&format=View+Data' tidesandcurrents.def
 
 # Read from compressed file.  Use default 'sparse.def' parsing definition file.
 ../tappy.py tridentpier_florida_8721604_data.txt.gz
 
 # Output
-../tappy.py -o mayport_florida_8720220_data.txt mayport_florida_8720220_data_def.txt
+../tappy.py --output mayport_florida_8720220_data.txt mayport_florida_8720220_data_def.txt
 
 # Test defining date as a single value.  Look at 
 # tridentpier_florida_8721604_datetime.txt.def for an explanation of what 
